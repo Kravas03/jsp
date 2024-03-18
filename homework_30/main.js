b01465363b01c108c912c866fd5b6b01f12af007
Andrey Kravchenko <148622608+Kravas03@users.noreply.github.com>
2024-03-18 12:59:09 +0200

    Delete homework_30 directory

--- homework_30/main.js
+++
@@ -1,4 +0,0 @@
-let userName = prompt("Enter your name");
-let userSurname = prompt("Enter your surname");
-let userAge = prompt("Enter your age");
-alert(`Is this information is correct: name: ${userName}, surname: ${userSurname}, age:${userAge} ?`);
