6984c72e4d19683689418e92db761982d990c93d
Andrey Kravchenko <148622608+Kravas03@users.noreply.github.com>
2024-04-11 12:40:10 +0300

    remade

--- Homework_16/main.js
+++ Homework_16/main.js
@@ -1,14 +1,14 @@
 let ladder = {
 	step: 0,
-	up: function () { 
+	up () { 
 		this.step++;
 		return this;
 	},
-	down: function () {
+	down () {
 		this.step--;
 		return this;
 	},
-	showStep: function () {
+	showStep () {
 		console.log(this.step);
 		return this;
 	},
