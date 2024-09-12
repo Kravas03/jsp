29af2a07241d30c406b1b85d864ddb5323cc9154
Andrey Kravchenko <148622608+Kravas03@users.noreply.github.com>
2024-09-12 15:43:14 +0300

    Add files via upload

---
+++ Homework_17.3/main.js
@@ -0,0 +1,25 @@
+class BankAccount {
+  constructor(balance) {
+    this.balance = balance;
+  }
+
+  deposit(value) {
+    this.balance += value;
+  }
+  withdraw(value) {
+    this.balance -= value;
+  }
+  getBalance() {
+    return this.balance;
+  }
+}
+const account1 = new BankAccount(1000);
+console.log(account1.getBalance()); // 1000
+
+account1.deposit(500);
+
+console.log(account1.getBalance()); // 1500
+
+account1.withdraw(200);
+
+console.log(account1.getBalance()); // 1300
